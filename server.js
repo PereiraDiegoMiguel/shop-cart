7179316f716f0d91664f5e6e39430bbe6468ed74
Diego Miguel Pereira <corgandp@gmail.com>
2020-09-05 16:46:36 -0300

    Created server.js

--- server.js
+++ server.js
@@ -4,20 +4,45 @@ const mongoose = require("mongoose");
 const shortid = require("shortid");
 
 const app = express();
-app.use(bodyParser);
+app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhhost.shop-cart", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
+
+mongoose.connect("mongodb://localhost/shop-cart",{
+  useNewUrlParser: true, 
+  useFindAndModify: false, 
+  useUnifiedTopology: true, 
+  useCreateIndex: true 
 });
+
 
 const Product = mongoose.model(
   "products",
   new mongoose.Schema({
-    _id: { type: shortid.generate },
+    _id: { type: String, default: shortid.generate },
+    title: String,
+    description: String,
+    image: String,
+    price: String,
+    availableSizes: [String],
   })
 );
 
-app.get("/api/products", (req, res) => { });
-// parei em 0943
+app.get("/api/products", async (req, res) => {
+  const products = await Product.find({});
+  res.send(products);
+});
+
+app.post("api/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  const savedProduct = await newProduct.save();
+  res.send(savedProduct);
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+  res.send(deletedProduct);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () =>
+  console.log("server at http://localhost:5000"));
